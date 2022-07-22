4d73a1e9f291c49f6d50c986a1b9884c8c64389b
mishalp <mishalclever@gmail.com>
2022-07-22 05:25:19 -0400

    canvas finished

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const pixel = document.createElement('div');
+const pixel = document.createElement('div').cloneNode(true);
 pixel.classList.add('pixel')
 //console.log(pixel);
 const canvas = document.getElementById('canvas')
 
-for(let i=0;i<256;i++){
-    canvas.appendChild(pixel);
+
+for(let i=0;i<255;i++){
+    canvas.appendChild(pixel.cloneNode(true))
 }
 
